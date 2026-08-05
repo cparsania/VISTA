1b06ec6f147d2127331875413a8f9b1f0ddf25b6
Chirag Parsania <chirag.parsania@gmail.com>
2026-08-05 14:22:39 +1000

    Import the base functions the code calls unqualified
    R CMD check listed callNextMethod, na.omit, as.dist, hclust,
    getFromNamespace and head as undefined globals. They resolved only
    because methods/stats/utils happen to be attached. callNextMethod is
    used by the new [ method, so this branch introduced that one.

--- R/VISTA-package.R
+++ R/VISTA-package.R
@@ -2,5 +2,8 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom methods callNextMethod
+#' @importFrom stats as.dist hclust na.omit
+#' @importFrom utils getFromNamespace head
 ## usethis namespace: end
 NULL
